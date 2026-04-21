b2f6c9528d2a7e7b4d04714b1748c3f417b87fdc
Dmitry Ulitin <dmitry.ulitin@gmail.com>
2026-04-22 00:12:43 +0300

    feat: update dependencies and improve app component tests

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { TUI_OPTIONS } from '@taiga-ui/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [{ provide:  TUI_OPTIONS, useValue: {} }],
     }).compileComponents();
   });
 
@@ -18,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, swarmer-finance');
+    expect(compiled.querySelector('span')?.textContent).toContain('Swarmer Finance');
   });
 });
